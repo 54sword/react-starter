5e1f6946281ef91766b15fe8d0d130bd30f7c62a
54sword <54sword@gmail.com>
2018-05-08 10:41:03 +0800

    开发环境显示redux日志

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import { createLogger } from 'redux-logger';
 let middleware = [ thunk ];
 
 // 如果是在客户端环境，并且是开发模式，那么打印redux日志
-// if (!process.env.__NODE__ && __DEV__) middleware.push(createLogger());
+if (!process.env.__NODE__ && __DEV__) middleware.push(createLogger());
 
 export default function configureStore(initialState) {
   const store = createStore(
